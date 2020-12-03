9d80665c322ff98358cb86fa077073b4ef718fb9
Valyukhov <azedval@gmail.com>
2020-12-03 21:27:45 +0300

    secondcommit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 "use strict";
 
 console.log ('arr'+ "- object");
-console.log (4 + "- object");
+console.log (4 +  "5");
+let incr = 10,
+    decr = 10;
+
+console.log(++incr);
+console.log(--decr);
+
+const isChecked = true,
+        isClose = false;
+console.log(isChecked && isClose);
+console.log("Hello World");
