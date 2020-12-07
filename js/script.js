b7d22a1922453bc5e1482e26ca1de386b3e649ac
Valyukhov <azedval@gmail.com>
2020-12-07 21:11:35 +0300

    objects

--- js/script.js
+++ js/script.js
@@ -73,27 +73,39 @@ function writeYourGenres() {
 writeYourGenres();
 
 
-function first() {
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
-
-function learnJS(lang,callback) {
-    console.log (`Я учу: ${lang}`);
-    callback();
-}
-function done() {
-    console.log('Я прошел этот урок');
-}
-
-learnJS('JavaScript',done);
+const {border, bg} = options.colors;
 
 
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+// console.log(options);
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`); 
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//     }
+    
+// }
